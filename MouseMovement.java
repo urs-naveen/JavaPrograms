099860c1f4e4a1a3bcb234c4be6859bb7f6b62fa
NAVEEN KUMAR <naveen.kasa1080@gmail.com>
2025-03-28 02:23:21 +0530

    Move screen size calculation inside loop
    Relocating the screen size calculation ensures the program adapts to any changes in screen dimensions dynamically. This adjustment resolves potential issues when screen size changes after the application starts.

--- MouseMovement.java
+++ MouseMovement.java
@@ -4,12 +4,12 @@ public class MouseMovement {
 
     public static void main(String[] args) throws AWTException {
         Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
-        int screenWidth = (int) screenSize.getWidth();
-        int screenHeight = (int) screenSize.getHeight();
-
         Robot bot = new Robot();
 
         while (true) {
+            int screenWidth = (int) screenSize.getWidth();
+            int screenHeight = (int) screenSize.getHeight();
+
             bot.delay(60000);
             bot.mouseMove(screenWidth, screenHeight);
         }
